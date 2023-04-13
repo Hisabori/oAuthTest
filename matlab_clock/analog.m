9a503cf3e1c628ee229566c8be935e44eaef02cc
Jeonghun HAN <101242920+Hisabori@users.noreply.github.com>
2023-04-13 23:48:05 +0900

    Update analog.m

--- matlab_clock/analog.m
+++ matlab_clock/analog.m
@@ -1,5 +1,6 @@
-%fix 23.04.13
-%23:24 bugfix
+-----이 라인은 지워 줘-----
+-----수정 다시 해 봄------
+-----11:47-----
 
 
 function drawClock()
@@ -12,10 +13,11 @@ function drawClock()
     drawClockFace();
 
     % Draw hour, minute, and second hands
-    global hourHandLength minuteHandLength secondHandLength;
+    global hourHandLength minuteHandLength secondHandLength hourHandWidth;
     hourHandLength = 0.5;
     minuteHandLength = 0.8;
     secondHandLength = 0.9;
+    hourHandWidth = 3; % Add hourHandWidth as a global variable
     drawHands(0, 0, 0);
 
     % Update position of hour, minute, and second hands every second
@@ -23,11 +25,11 @@ function drawClock()
         t = clock();
         updateTime(mod(t(4), 12), t(5), t(6));
         pause(0.01);
-    endwhile
-endfunction
+    end
+end
 
 function updateTime(hour, minute, second)
-    global hourHandLength minuteHandLength secondHandLength;
+    global hourHandLength minuteHandLength secondHandLength hourHandWidth;
 
     clf;
     axis([-1 1 -1 1], "square");
@@ -35,40 +37,8 @@ function updateTime(hour, minute, second)
 
     drawClockFace();
 
-    hourAngle = (30*(mod(hour, 12) + minute/60))/180*pi;
-    minuteAngle = (6*(mod(minute, 60) + second/60))/180*pi;
-    secondAngle = (6*second)/180*pi;
+hourAngle = (30*(mod(hour, 12) + minute/60))/180*pi;
+minuteAngle = (6*(mod(minute, 60) + second/60))/180*pi;
+secondAngle = (6*second)/180*pi;
 
 drawHands(hourAngle, minuteAngle, secondAngle);
-endfunction
-
-function drawHands(hourAngle, minuteAngle, secondAngle)
-    global hourHandLength minuteHandLength secondHandLength;
-
-    % Draw hour hand
-    hourHand = [0 0; hourHandLength*cos(hourAngle - pi/2) hourHandLength*sin(hourAngle - pi/2)];
-    line(hourHand(:,1), hourHand(:,2), "linewidth", 3);
-
-    % Draw minute hand
-    minuteHand = [0 0; minuteHandLength*cos(minuteAngle - pi/2) minuteHandLength*sin(minuteAngle - pi/2)];
-    line(minuteHand(:,1), minuteHand(:,2), "linewidth", 2);
-
-    % Draw second hand
-    secondHand = [0 0; secondHandLength*cos(secondAngle - pi/2) secondHandLength*sin(secondAngle - pi/2)];
-    line(secondHand(:,1), secondHand(:,2), "linewidth", 1, "color", "red");
-endfunction
-
-% Refresh time every second
-while (1)
-pause(0.1);
-t = clock;
-
-% Calculate position of hour, minute, and second hands
-hourAngle = (30*(mod(t(4),12) + t(5)/60))/180*pi;
-minuteAngle = (6*(mod(t(5),60) + t(6)/60))/180*pi;
-secondAngle = (6*t(6))/180*pi;
-
-% Calculate position of hour, minute, and second hands and draw them
-hourHand = createHand(hourAngle, hourHandLength, hourHandWidth);
-plot(hourHand(:,1), hourHand(:,2),'k','LineWidth',hourHandWidth);
-end
